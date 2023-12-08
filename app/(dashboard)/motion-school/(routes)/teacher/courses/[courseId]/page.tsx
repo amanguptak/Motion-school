1d7031771c9c49060f91be4885828fa6e4f8224a
Aman Gupta <eagleax7@gmail.com>
2023-12-08 15:17:33 +0530

    corse-detail

--- app/(dashboard)/motion-school/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/motion-school/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,15 +1,69 @@
-import React from 'react'
-
-interface ParamsType{
-  params:{
-    courseId : string
-  } 
+import React from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { CustomIcon } from "@/components/custom-icon";
+import { LayoutDashboard } from "lucide-react";
+interface ParamsType {
+  params: {
+    courseId: string;
+  };
 }
 
-const CourseId = ({params}:ParamsType) => {
+const CourseId = async ({ params }: ParamsType) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedField = requiredFields.filter(Boolean).length;
+  const progressText = `(${completedField}/${totalFields})`;
+
   return (
-    <div>CourseId:{params.courseId}</div>
-  )
-}
+    <div className=" lg:h-[600px] p-7   m-8 h-[700px] mx-5 lg:mx-auto shadow-lg items-center justify-center max-w-3xl  bg-indigo-400  rounded-md bg-clip-padding backdrop-filter lg:backdrop-blur-sm bg-opacity-20 border border-gray-100">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-semibold text-slate-800">
+            Course SetUp
+          </h1>
+          <span className="text-sm text-slate-600">
+            Please Complete all steps {progressText}{" "}
+          </span>
+        </div>
+      </div>
 
-export default CourseId
+      {/* course form setup */}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-16">
+        <div className="flex items-center gap-x-2">
+          <CustomIcon size='md' icon={LayoutDashboard}/>
+          <h3 className="text-lg text-slate-800">
+            {" "}
+            Customize Your Course Here
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseId;
--- components/custom-icon.tsx
+++ components/custom-icon.tsx
@@ -1,5 +1,5 @@
 import { LucideIcon } from "lucide-react";
-import { cva, type VariantProps }  from "class-variance-authority";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
@@ -8,56 +8,53 @@ const backgroundVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-sky-100",
+        default: "bg-indigo-600",
         success: "bg-emerald-100",
+
       },
       size: {
-        default: "p-2",
+        default: "p-3",
+        md:"p-2",
         sm: "p-1",
-      }
+       
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
-    }
+    },
   }
 );
 
-const iconVariants = cva(
-  "",
-  {
-    variants: {
-      variant: {
-        default: "text-sky-700",
-        success: "text-emerald-700",
-      },
-      size: {
-        default: "h-8 w-8",
-        sm: "h-4 w-4"
-      },
+const iconVariants = cva("", {
+  variants: {
+    variant: {
+      default: "text-amber-300",
+      success: "text-emerald-700",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    }
-  }
-);
+    size: {
+      default: "h-8 w-8",
+      md:"h-6 w-6",
+      sm: "h-4 w-4",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 type BackgroundVariantsProps = VariantProps<typeof backgroundVariants>;
 type IconVariantsProps = VariantProps<typeof iconVariants>;
 
 interface CustomIconProps extends BackgroundVariantsProps, IconVariantsProps {
   icon: LucideIcon;
-};
+}
 
-export const CustomIcon = ({
-  icon: Icon,
-  variant,
-  size,
-}: CustomIconProps) => {
+export const CustomIcon = ({ icon: Icon, variant, size }: CustomIconProps) => {
   return (
     <div className={cn(backgroundVariants({ variant, size }))}>
       <Icon className={cn(iconVariants({ variant, size }))} />
     </div>
-  )
+  );
 };
