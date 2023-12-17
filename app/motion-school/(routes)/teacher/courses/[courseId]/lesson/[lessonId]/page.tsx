e89495b095c99e737ba6cb0c2286f292857dbd10
Aman Gupta <eagleax7@gmail.com>
2023-12-17 20:45:34 +0530

    alert added

--- app/motion-school/(routes)/teacher/courses/[courseId]/lesson/[lessonId]/page.tsx
+++ app/motion-school/(routes)/teacher/courses/[courseId]/lesson/[lessonId]/page.tsx
@@ -1,14 +1,20 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft } from "lucide-react";
+import { AlertTriangle, ArrowLeft, Trash } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
+
+import { Terminal } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 import { CustomIcon } from "@/components/custom-icon";
 import LessonInfo from "./_components/lessonInfo";
 import LessonAccess from "./_components/lessonaccess";
 import LessonVideo from "./_components/lessonvideo";
 import LessonYoutube from "./_components/lessonyoutube";
+import { Button } from "@/components/ui/button";
 
 interface LessonProps {
   params: {
@@ -45,62 +51,82 @@ const Lesson = async ({ params }: LessonProps) => {
 
   const isCompleted = requiredFields.every(Boolean);
   return (
-    <div className="h-fit space-y-2  lg:scrollbar-track-indigo-500 p-7   mt-4 m-7 lg:mx-auto shadow-lg items-center justify-center max-w-5xl  bg-indigo-400  rounded-md bg-clip-padding backdrop-filter lg:backdrop-blur-sm bg-opacity-20 border border-gray-100">
-      <div>
-        <Link
-          className="flex items-center space-x-2 cursor-pointer"
-          href={`/motion-school/teacher/courses/${params.courseId}`}
-        >
-          <ArrowLeft
-            size={20}
-            className="text-indigo-500 font-medium hover:text-indigo-700"
-          />
-          <span className="text-sm text-slate-800 font-medium">
-            {" "}
-            Back to Course
-          </span>
-        </Link>
-      </div>
+    <>
+      {!lesson.isPublished && (
+        <Alert>
+          <div className="flex items-center space-x-2">
+            <AlertTriangle size={16} className="text-blue-700" />
 
-      <div className="flex flex-col gap-y-1">
-        <h1 className="text-2xl font-semibold text-slate-800">Lesson SetUp</h1>
-        <span className="text-sm text-slate-600">
-          Please Complete all steps {progressText}{" "}
-        </span>
-      </div>
+            <AlertDescription>
+              This lesson is unpublished. It will not be visible in the course
+            </AlertDescription>
+          </div>
+        </Alert>
+      )}
 
-      <div className="grid md:grid-cols-12 grid-cols-1 mt-6 space-y-4 md:space-y-0  lg:gap-x-16">
-
-        <div className="lg:col-span-6 col-span-12">
-        <LessonInfo
-          initialData={lesson}
-          courseId={params.courseId}
-          lessonId={params.lessonId}
-        />
-
-        <LessonAccess
-         initialData={lesson}
-         courseId={params.courseId}
-         lessonId={params.lessonId}
-        />
+      <div className="h-fit space-y-2  lg:scrollbar-track-indigo-500 p-7   mt-2 lg:m-6 m-4 lg:mx-auto shadow-lg items-center justify-center max-w-5xl  bg-indigo-400  rounded-md bg-clip-padding backdrop-filter lg:backdrop-blur-sm bg-opacity-20 border border-gray-100">
+        <div>
+          <Link
+            className="flex items-center space-x-2 cursor-pointer"
+            href={`/motion-school/teacher/courses/${params.courseId}`}
+          >
+            <ArrowLeft
+              size={20}
+              className="text-indigo-500 font-medium hover:text-indigo-700"
+            />
+            <span className="text-sm text-slate-800 font-medium">
+              {" "}
+              Back to Course
+            </span>
+          </Link>
         </div>
-        <div className="col-span-12 lg:col-span-6">
-          <LessonVideo
-            initialData={lesson}
-            courseId={params.courseId}
-            lessonId={params.lessonId}
-          />
 
-          <LessonYoutube
+        <div className="flex gap-y-1 items-center justify-between">
+         <div>
+         <h1 className="text-2xl font-semibold text-slate-800">
+            Lesson SetUp
+          </h1>
+          <span className="text-sm text-slate-600">
+            Please Complete all steps {progressText}{" "}
+          </span>
+         </div>
+
+          <div className="flex items-center space-x-2">
+            <Button size="sm"> Publish </Button>
+            <Trash size={20} className="text-red-600"/>
+          </div>
+        </div>
+
+        <div className="grid md:grid-cols-12 grid-cols-1 mt-6 space-y-4 md:space-y-0  lg:gap-x-16">
+          <div className="lg:col-span-6 col-span-12">
+            <LessonInfo
               initialData={lesson}
               courseId={params.courseId}
               lessonId={params.lessonId}
             />
-       </div>
-      </div>
 
-     
-    </div>
+            <LessonAccess
+              initialData={lesson}
+              courseId={params.courseId}
+              lessonId={params.lessonId}
+            />
+          </div>
+          <div className="col-span-12 lg:col-span-6">
+            <LessonVideo
+              initialData={lesson}
+              courseId={params.courseId}
+              lessonId={params.lessonId}
+            />
+
+            <LessonYoutube
+              initialData={lesson}
+              courseId={params.courseId}
+              lessonId={params.lessonId}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
