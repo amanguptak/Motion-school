a007c12ce05010e2da8d61ca37ce3df815b2bff0
Aman Gupta <eagleax7@gmail.com>
2023-12-31 20:20:50 +0530

    progress bug fixed

--- app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -2,40 +2,38 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export const PUT = async(req:Request , {params}:{params:{courseId : string , chapterId : string}})=>{
+export const PUT = async (
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) => {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
 
-try{
-const {userId} = auth()
-if(!userId) return new NextResponse("Unauthorized",{status:401})
-
-    const {isCompleted} = await req.json();
+    const { isCompleted } = await req.json();
     // "Upsert" is a database operation that combines the actions of "update" and "insert." The term "upsert" is a portmanteau of "update" and "insert." This operation is useful when you want to either update an existing record in the database or insert a new record if the specified conditions do not match any existing records.
     const userProgress = await db.userProgress.upsert({
-        where:{
-            userId_chapterId:{
-                userId: userId,
-                chapterId: params.chapterId
-            }
+      where: {
+        userId_chapterId: {
+          userId: userId,
+          chapterId: params.chapterId,
         },
+      },
 
-        update:{
-            isCompleted
-        },
+      update: {
+        isCompleted,
+      },
 
-        create:{
-            userId,
-            chapterId: params.chapterId,
-            isCompleted,
-        }
-    }
-      
-    )
+      create: {
+        userId,
+        chapterId: params.chapterId,
+        isCompleted,
+      },
+    });
 
-    return NextResponse.json(userProgress)
-
-}catch(err){
-    console.log("progress",err);
-    return new NextResponse("Internal Server Error" , {status: 500})
-}
-
-}
+    return NextResponse.json(userProgress);
+  } catch (err) {
+    console.log("progress", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
--- app/course/[courseId]/lesson/[lessonId]/_components/CompleteLesson.tsx
+++ app/course/[courseId]/lesson/[lessonId]/_components/CompleteLesson.tsx
@@ -1,20 +1,60 @@
-import React from 'react'
-import { Button } from '@/components/ui/button';
-import axios from 'axios';
+"use client";
 
-interface CompleteLessonProps{
-    courseId : string;
-    lessonId : string;
-    isCompleted : boolean;
-    nextChapterId?: string;
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { CheckCircle, XCircle } from "lucide-react";
+// import { useConfettiStore } from "@/hooks/use-confetti-store";
+interface CompleteLessonProps {
+  courseId: string;
+  lessonId: string;
+  isCompleted: boolean;
+  nextChapterId?: string;
 }
 
-const CompleteLesson = ({courseId,lessonId,isCompleted,nextChapterId}:CompleteLessonProps) => {
+const CompleteLesson = ({
+  courseId,
+  lessonId,
+  isCompleted,
+  nextChapterId,
+}: CompleteLessonProps) => {
+  const router = useRouter();
+  //   const confetti = useConfettiStore()
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onComplete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/${lessonId}/progress`, {
+        isCompleted: !isCompleted
+      });
+      toast.success("Progress updated");
+      router.refresh();
+    } catch (err) {
+      toast.error("Something went wrong");
+      console.error(err)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const Icon = isCompleted ? XCircle : CheckCircle;
   return (
     <div>
-        <Button>Completed</Button>
+      <Button
+        onClick={onComplete}
+        disabled={isLoading}
+        type="button"
+        className="w-full md:w-auto"
+      >
+        {" "}
+        {isCompleted ? "Not completed" : "Mark as complete"}
+        <Icon className="h-4 w-4 ml-2" />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default CompleteLesson
+export default CompleteLesson;
