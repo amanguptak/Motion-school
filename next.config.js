9c5cae9d82572a1b9854bae70f10b5e5559238df
Aman Gupta <eagleax7@gmail.com>
2024-01-12 21:06:34 +0530

    added-pwa feature

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontendNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig = {
-    images: {
-      domains: [
-        "utfs.io"
-      ]
-    }
-  }
-  
-  module.exports = nextConfig
+  images: {
+    domains: ["utfs.io"],
+  },
+};
+
+module.exports = withPWA(nextConfig);
